5ca89fd8b389443aaff68c71ec05a87d57f7e798
Zheng Jie <3492637078@qq.com>
2026-01-19 01:50:27 +0000

    refactor: 删除账户相关的应用服务和导出文件

--- packages/domain-client/src/account/application/index.ts
+++
@@ -1,6 +0,0 @@
-/**
- * Account Application Layer
- * 账户模块应用层导出
- */
-
-export * from './services';
--- packages/domain-client/src/account/application/services/index.ts
+++
@@ -1,7 +0,0 @@
-/**
- * Account Application Services
- * 账户模块应用层服务导出
- */
-
-export { AccountProfileApplicationService } from './AccountProfileApplicationService';
-export { AccountSubscriptionApplicationService } from './AccountSubscriptionApplicationService';
