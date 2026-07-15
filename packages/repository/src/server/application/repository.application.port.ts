6b935978b10e7be8c3af98c5a1f7a5bf5778eb19
BakerSean168 <lulouhao@stu.sicau.edu.cn>
2026-07-15 11:47:15 +0000

    fix(repository): preserve resource MIME type for indexing

--- packages/repository/src/server/application/repository.application.port.ts
+++ packages/repository/src/server/application/repository.application.port.ts
@@ -19,6 +19,7 @@ export interface RepositoryApplicationPort {
       type: string;
       path?: string;
       content?: string;
+      mimeType?: string;
     },
     ctx: Context,
   ): Promise<Result<ResourceClientDTO>>;
